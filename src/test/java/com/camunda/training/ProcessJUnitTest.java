513f2fb5d1f44ab1d0ba9ff0862affa5333fe95d
Guilherme de Souza (hguigit) <guilherme.silva@technosoftware.com.br>
2025-08-08 00:11:18 -0300

    feat: adding happy test to tweet camunda

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,57 @@
 package com.camunda.training;
 
+import org.assertj.core.api.Assertions;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
+import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.withVariables;
+
+@ExtendWith(ProcessEngineExtension.class)
 public class ProcessJUnitTest {
-  
-//  @Test
-  public void testHappyPath() {
-    
-  }
+
+    @Test
+    @Deployment(resources = "META-INF/TweetDiagram.bpmn")
+    public void testHappyPath(ProcessEngine processEngine) {
+
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        TaskService taskService = processEngine.getTaskService();
+
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey("WriteAndPostTweet");
+        BpmnAwareTests.assertThat(pi).isNotNull();
+        BpmnAwareTests.assertThat(pi).isStarted();
+
+        Task write = taskService.createTaskQuery()
+                .processInstanceId(pi.getId())
+                .taskName("Write Tweet")
+                .singleResult();
+        BpmnAwareTests.assertThat(write).isNotNull();
+
+        taskService.complete(write.getId());
+
+        Task review = taskService.createTaskQuery()
+                .processInstanceId(pi.getId())
+                .taskName("Review Tweet")
+                .singleResult();
+        BpmnAwareTests.assertThat(review).isNotNull();
+
+        taskService.complete(review.getId(),
+                withVariables("tweetAproved", true));
+
+        BpmnAwareTests.assertThat(pi).isEnded();
+        Assertions.assertThat(processEngine.getHistoryService()
+                .createHistoricActivityInstanceQuery()
+                .processInstanceId(pi.getId())
+                .activityName("Tweet posted")
+                .count()).isEqualTo(1);
+
+    }
 
 }
